55fe00bd0d52c1febf1d2a9220b90215726893e5
sadia1120 <sadiashomsher11@gmail.com>
2026-04-21 22:17:08 +0600

    Add Alzheimer Detection App using Streamlit and TensorFlow

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+
+model = tf.keras.models.load_model("model.keras", compile=False)
+
+classes = ['MildDemented','ModerateDemented','NonDemented','VeryMildDemented']
+
+st.title("🧠 Alzheimer Detection App")
+
+file = st.file_uploader("Upload MRI", type=["jpg","png","jpeg"])
+
+if file:
+    img = Image.open(file)
+    st.image(img)
+
+    img = img.resize((128,128))
+    img = image.img_to_array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+
+    pred = model.predict(img)
+    st.write("Prediction:", classes[np.argmax(pred)])
+    st.write("Confidence:", np.max(pred))
