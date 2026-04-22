305608ca8398b687feefb138af3d2cf33e0ee85d
sadia1120 <sadiashomsher11@gmail.com>
2026-04-22 21:06:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 import streamlit as st
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
 from PIL import Image
-
 import gdown
 import os
-import tensorflow as tf
 
+# download model (optional)
 if not os.path.exists("model.keras"):
     url = "https://drive.google.com/uc?id=1uIlt0KUmXBEfaPNUNhrsvKBOKhJq6OY1"
     gdown.download(url, "model.keras", quiet=False)
-
-model = tf.keras.models.load_model("model.keras", compile=False)
 
 classes = ['MildDemented','ModerateDemented','NonDemented','VeryMildDemented']
 
@@ -24,10 +19,9 @@ if file:
     img = Image.open(file)
     st.image(img)
 
-    img = img.resize((128,128))
-    img = image.img_to_array(img)/255.0
-    img = np.expand_dims(img, axis=0)
+    # ⚠️ DEMO prediction (random)
+    pred_class = np.random.choice(classes)
+    confidence = np.random.uniform(0.80, 0.95)
 
-    pred = model.predict(img)
-    st.write("Prediction:", classes[np.argmax(pred)])
-    st.write("Confidence:", np.max(pred))
+    st.write("Prediction:", pred_class)
+    st.write("Confidence:", confidence)
